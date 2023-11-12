2685b6a90f09fc85214e8e9cd99510d73ad28b49
Caio <caio@CAIO.localdomain>
2023-11-12 16:07:26 -0300

    feat: initial structure for the project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h2 class="text-h3 text-center pa-12">MovieSearch {{ apiKey }}</h2>
+    <v-row>
+      <v-text-field
+        v-model="title"
+        label="Search"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  const apiKey = process.env.VUE_APP_API_KEY
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    data: () => ({
+      title: '',
+      apiKey,
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+    }),
   }
-}
 </script>
